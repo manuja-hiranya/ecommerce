61604fc1e7c92651a7a81cf4fac24d1f644976f2
Manuja Hiranya <manuja.hiranya8@gmail.com>
2025-03-26 18:19:36 +0530

    updated

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from store.views import home  # Import the view
+from store.views import home, shop, beauty, spices, fashion, contact, about  # Import the views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),  # Add this route
+    path('', home, name='home'),
+    path('shop/', shop, name='shop'),
+    path('categories/beauty/', beauty, name='beauty'),
+    path('categories/spices/', spices, name='spices'),
+    path('categories/fashion/', fashion, name='fashion'),
+    path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
 ]
--- store/views.py
+++ store/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'store/index.html')
+
+def shop(request):
+    return render(request, 'store/shop.html')
+
+def beauty(request):
+    return render(request, 'store/beauty.html')
+
+def spices(request):
+    return render(request, 'store/spices.html')
+
+def fashion(request):
+    return render(request, 'store/fashion.html')
+
+def contact(request):
+    return render(request, 'store/contact.html')
+
+def about(request):
+    return render(request, 'store/about.html')
